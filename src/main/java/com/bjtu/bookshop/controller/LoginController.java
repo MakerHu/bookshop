fcdf17e05a75de01f7a6fcc1ee5689409883e77b
MakerHu <1003964217@qq.com>
2021-04-17 11:53:13 +0800

    胡江浩 实现登录注册的加密解密功能

--- src/main/java/com/bjtu/bookshop/controller/LoginController.java
+++ src/main/java/com/bjtu/bookshop/controller/LoginController.java
@@ -9,14 +9,50 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.Resource;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.security.SecureRandom;
 
 @Controller
 //@RequestMapping("/login")
 public class LoginController {
     @Resource
     private UserService userService;
+
+
+    private static final byte[] DES_KEY = { 21, 1, -110, 82, -32, -85, -128, -65 };
+
+
+    /**
+     * 解密方法
+     * @param cryptData
+     * @return
+     */
+    @SuppressWarnings("restriction")
+    public static String decryptBasedDes(String cryptData) {
+        String decryptedData = null;
+        try {
+            // DES算法要求有一个可信任的随机数源
+            SecureRandom sr = new SecureRandom();
+            DESKeySpec deskey = new DESKeySpec(DES_KEY);
+            // 创建一个密匙工厂，然后用它把DESKeySpec转换成一个SecretKey对象
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey key = keyFactory.generateSecret(deskey);
+            // 解密对象
+            Cipher cipher = Cipher.getInstance("DES");
+            cipher.init(Cipher.DECRYPT_MODE, key, sr);
+            // 把字符串进行解码，解码为为字节数组，并解密
+            decryptedData = new String(cipher.doFinal(new sun.misc.BASE64Decoder().decodeBuffer(cryptData)));
+        } catch (Exception e) {
+            throw new RuntimeException("解密错误，错误信息：", e);
+        }
+        return decryptedData;
+    }
+
 
     @GetMapping(value = "/login")
     public String getLogin(Model model) {
@@ -32,7 +68,7 @@ public class LoginController {
 //            System.out.println();
             User user = userService.findByEmail(msgUser.getEmail());
             if (user != null) {
-                if (user.getPassword().equals(msgUser.getPassword())) {
+                if (decryptBasedDes(user.getPassword()).equals(msgUser.getPassword())) {
                     //消除返回前端的收能过户数据中的重要信息
                     user.setPassword("");
                     HttpSession session = request.getSession();
--- src/main/java/com/bjtu/bookshop/controller/RegisterController.java
+++ src/main/java/com/bjtu/bookshop/controller/RegisterController.java
@@ -10,12 +10,46 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.Resource;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import java.security.SecureRandom;
 
 @Controller
 public class RegisterController {
 
     @Resource
     private UserService userService;
+
+
+    private static final byte[] DES_KEY = { 21, 1, -110, 82, -32, -85, -128, -65 };
+    /**
+     * 加密方法
+     * @param data
+     * @return
+     */
+    @SuppressWarnings("restriction")
+    public static String encryptBasedDes(String data) {
+        String encryptedData = null;
+        try {
+            // DES算法要求有一个可信任的随机数源
+            SecureRandom sr = new SecureRandom();
+            DESKeySpec deskey = new DESKeySpec(DES_KEY);
+            // 创建一个密匙工厂，然后用它把DESKeySpec转换成一个SecretKey对象
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey key = keyFactory.generateSecret(deskey);
+            // 加密对象
+            Cipher cipher = Cipher.getInstance("DES");
+            cipher.init(Cipher.ENCRYPT_MODE, key, sr);
+            // 加密，并把字节数组编码成字符串
+            encryptedData = new sun.misc.BASE64Encoder().encode(cipher.doFinal(data.getBytes()));
+        } catch (Exception e) {
+            // log.error("加密错误，错误信息：", e);
+            throw new RuntimeException("加密错误，错误信息：", e);
+        }
+        return encryptedData;
+    }
 
     @GetMapping(value = "/register")
     public String register(Model model){
@@ -31,6 +65,8 @@ public class RegisterController {
             return "register";
         }
 
+        String encryptPassword = encryptBasedDes(user.getPassword());
+        user.setPassword(encryptPassword);
         user.setRole("customer");
         userService.add(user);
         model.addAttribute("msg","注册成功！");
